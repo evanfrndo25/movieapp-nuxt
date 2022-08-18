6c8544501da5aeca6717bf9858c6cf013ab46d56
evanfrndo25 <evansfernando25@gmail.com>
2022-08-18 11:29:09 +0700

    firstcommit

---
+++ components/LoadingComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="loading">
+        <span></span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoadingComponent'
+}
+</script>
+
+
+<style lang="scss" scoped>
+.loading{
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @keyframes circle-loading {
+        to{
+            transform: rotateZ(360deg);
+        }
+    }
+}
+</style>
